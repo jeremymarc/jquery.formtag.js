d90a07df7131ec8601a34d7728c60aeda2021518
Jeremy Marc <jeremy.marc@me.com>
2014-05-08 23:38:53 -0700

    add hidden class to play nicely with css animation

--- jquery.formtag.js
+++ jquery.formtag.js
@@ -5,8 +5,7 @@
  *  (c) 2014-2015 http://jeremymarc.github.com/jquery.formtag.js/
  *  MIT licensed
  */
-
-(function (factory) {
+;(function (factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD
     define(['jquery'], factory);
@@ -19,8 +18,10 @@
   }
 }(function ($) {
   var formElements = [],
+      $wrapper,
       $addMenu,
       $tags,
+      $addMenuWrapper,
       $addTagLink;
 
   var formTag = function() {
@@ -32,6 +33,8 @@
         add_button_class: 'add_field',
         tag_label: '~',
         namespace: 'formTag-',
+        delete_tag_delay: 0,
+        add_tag_delay: 0,
       };
 
       /**
@@ -50,26 +53,32 @@
           formElements.push($(formElement));
         });
 
+        $wrapper = $('<div/>').addClass(_.options.namespace + 'wrapper');
+
         // Add Tag Link
         $addTagLink = $('<a/>');
         $addTagLink.html(_.options.add_button_text).addClass(_.options.namespace + _.options.add_button_class);
         $addTagLink.click(function() {
           updateAddMenuElementsVisibility();
+          return false;
         });
 
         // Generate tags for form elements set
         $tags = $('<div/>').addClass(_.options.namespace + 'tags');
         _.tag.init();
 
-        $form.after($tags);
-        $tags.after($addTagLink);
+
+        $form.after($wrapper);
+        $wrapper.append($tags);
         _.initMenu($form);
+        $addMenuWrapper.append($addTagLink);
 
         // close tag menu on click
         $(document).click(function(e) {
           if (0 === $(e.target).parents('.' + _.options.namespace + 'values').length) {
             $('.' + _.options.namespace + 'values').hide();
           }
+          $addMenu.addClass(_.options.namespace + 'hidden');
         });
 
         return _;
@@ -79,10 +88,13 @@
        * Build the add element menu from form labels.
        */
       _.initMenu = function($form) {
-        $addMenu = $('<ul/>').addClass(_.options.namespace + 'menu');
-        $addMenu.hide();
+        $addMenu = $('<ul/>')
+          .addClass(_.options.namespace + 'menu')
+          .addClass(_.options.namespace + 'hidden');
 
         var val;
+        $addMenuWrapper = $('<div/>').addClass(_.options.namespace + 'menu-wrapper');
+
         $(formElements)
         .each(function(i, formElement) {
           val = $(formElement).val();
@@ -93,13 +105,14 @@
 
           $addMenu.append($li);
         });
-        $tags.after($addMenu);
+        $addMenuWrapper.append($addMenu);
+        $tags.after($addMenuWrapper);
 
         $addMenu.on('click', function(e) {
           _.tag.add($(e.target).data());
 
           $('.' + _.options.namespace + 'tag:last-child ul').show();
-          $addMenu.hide();
+          $addMenu.addClass(_.options.namespace + 'hidden')
 
           //all form elements are displayed
           if (formElements.length == $tags.find('.' + _.options.namespace + 'tag').length) {
@@ -126,7 +139,10 @@
 
         add: function($formElement) {
           var $div, $a, $close, $tagMenu, label, $e;
-          $div = $('<div/>').addClass(_.options.namespace + 'tag').data($formElement);
+          $div = $('<div/>')
+            .addClass(_.options.namespace + 'tag')
+            .addClass(_.options.namespace + 'hidden')
+            .data($formElement);
           $formElement.addClass(_.options.namespace + 'tagged');
 
           //need to update the tag element
@@ -209,7 +225,10 @@
               .removeClass(_.options.namespace + 'tagged')
               .trigger('change');
 
-            $target.parent().remove();
+            $target.parent().addClass(_.options.namespace + 'hidden');
+            setTimeout(function() {
+              $target.parent().remove();
+            }, _.options.delete_tag_delay);
 
             $addTagLink.show();
           });
@@ -218,6 +237,9 @@
           $div.append($close);
           $div.append($tagMenu);
           $tags.append($div);
+          setTimeout(function() {
+            $div.removeClass(_.options.namespace + 'hidden');
+          }, _.options.add_tag_delay);
         },
 
         /**
@@ -298,7 +320,7 @@
       }
     });
 
-    $addMenu.show();
+    $addMenu.toggleClass('formTag-hidden');
   };
 
   function isTagShown($element) {
@@ -308,11 +330,11 @@
   function isFormElementValueValid(value) {
     return null !== value &&
       "undefined" !== typeof(value) &&
-      value.length > 0 && 
+      value.length > 0 &&
       "?" != value;
   };
 
-  $.fn.formtag = function (options) {
+  $.fn.formtag = function(options) {
     var len = this.length;
 
     return this.each(function(index) {
